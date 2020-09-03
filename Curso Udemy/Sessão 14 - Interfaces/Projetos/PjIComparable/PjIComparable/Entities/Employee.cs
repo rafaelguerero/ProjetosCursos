87614899b606cb2f46b55e5833858bb94b3a210b
rafaelguerero <rmichelinig@gmail.com>
2020-09-03 00:36:18 -0300

    Interface IComparable

---
+++ Curso Udemy/Sessão 14 - Interfaces/Projetos/PjIComparable/PjIComparable/Entities/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace PjIComparable.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vet = csvEmployee.Split(',');
+            Name = vet[0];
+            Salary = double.Parse(vet[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2",CultureInfo.InvariantCulture); 
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not a employee");
+            }
+            else
+            {
+                //Ordena a lista de funcionários pelo nome
+                Employee other = obj as Employee;
+                return Name.CompareTo(other.Name);
+            }
+        }
+    }
+}
---
+++ Curso Udemy/Sessão 14 - Interfaces/Projetos/PjIComparable/PjIComparable/Program.cs
@@ -0,0 +1,40 @@
+﻿using PjIComparable.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PjIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\in.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
