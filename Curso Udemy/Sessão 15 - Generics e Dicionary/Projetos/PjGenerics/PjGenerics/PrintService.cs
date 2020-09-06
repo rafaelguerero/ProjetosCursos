36a9276d25b63798090de937e6866c031e8e8166
rafaelguerero <rmichelinig@gmail.com>
2020-09-06 18:25:12 -0300

    Adicionando capítulo 15

---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjGenerics/PjGenerics/PrintService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PjGenerics
+{
+    class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int count = 0;
+
+        public void AddValue(T value)
+        {
+            if (count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full!");
+            }
+            _values[count] = value;
+            count++;
+        }
+
+        public T First()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty!");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (count > 0)
+            {
+                Console.Write(_values[count - 1]);
+            }
+            Console.Write("]");
+            Console.WriteLine();
+            Console.WriteLine("First: " + First());
+        }
+    }
+}
---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjGenerics/PjGenerics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PjGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> ps = new PrintService<int>();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                ps.AddValue(x);
+            }
+            ps.Print();
+        }
+    }
+}
