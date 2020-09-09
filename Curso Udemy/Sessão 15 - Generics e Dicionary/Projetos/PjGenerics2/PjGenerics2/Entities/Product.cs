350bd9e3a2b82025cb4d6e625bc07ad1d6bcc4e9
rafaelguerero <rmichelinig@gmail.com>
2020-09-08 23:26:20 -0300

    Restrições para Generics

---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjGenerics2/PjGenerics2/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace PjGenerics2.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Price: " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: ");
+            }
+            Product other = obj as Product;
+
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjGenerics2/PjGenerics2/Program.cs
@@ -0,0 +1,33 @@
+﻿using PjGenerics2.Entities;
+using PjGenerics2.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace PjGenerics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.WriteLine("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(", ");
+                string name = vect[0];
+                double price = double.Parse(vect[1],CultureInfo.InvariantCulture);
+                
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max: " + max);
+        }
+    }
+}
