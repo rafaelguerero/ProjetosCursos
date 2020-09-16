c5d15a6a786df2c519352eec4345e963fda11f1f
rafaelguerero <rmichelinig@gmail.com>
2020-09-15 23:23:30 -0300

    Aulas adicionais, cap 15.

---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjHash/PjHash/Program.cs
@@ -0,0 +1,33 @@
+﻿using PjHash.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace PjHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>
+            {
+                new Product("TV", 900.50),
+                new Product("Notebook", 2500.00)
+            };
+
+            HashSet<Point> b = new HashSet<Point>
+            {
+                new Point(3, 4),
+                new Point(5, 10)
+            };
+
+            Product prod = new Product("Notebook", 2500.00);
+
+            Console.WriteLine(a.Contains(prod));
+
+            //Struct compara por conteúdo e não por referência
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
+
+        }
+    }
+}
---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjHashSorted/PjHashSorted/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PjHashSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+            SortedSet<int> a = new SortedSet<int> { 0, 2, 4, 5, 7, 10 };
+            SortedSet<int> b = new SortedSet<int> { 1, 3, 6, 8, 9, 10 , 11 };
+            SortedSet<int> c = new SortedSet<int> (a);
+
+            //Une o conjunto c com o b
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            //intersecção
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            //Diferença
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
