e969c27249ed34865b2c10d81a2ccfff9dff6f8a
rafaelguerero <rmichelinig@gmail.com>
2020-10-09 00:19:26 -0300

    Finalizando cap. 15

---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Exercícios/ExDictionary/ExDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ExDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\eleicao.txt";
+
+            Dictionary<string, int> candidato = new Dictionary<string, int>();
+
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] c = sr.ReadLine().Split(',');
+                    string name = c[0];
+                    int votes = int.Parse(c[1]);
+
+                    if (candidato.ContainsKey(name))
+                    {
+                        candidato[name] += votes;
+                    }
+                    else
+                    {
+                        candidato[name] = votes;
+                    }
+                }
+                foreach (var item in candidato)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Curso Udemy/Sessão 15 - Generics e Dicionary/Projetos/PjDictionary/PjDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace PjDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> coockies = new Dictionary<string, string>();
+            coockies["User"] = "Maria";
+            coockies["email"] = "maria@gmail.com";
+            coockies["fone"] = "998547815";            
+
+            Console.WriteLine(coockies["fone"]);
+            Console.WriteLine(coockies["email"]);
+
+            coockies.Remove("email");
+            if (coockies.ContainsKey("email"))
+            {
+                Console.WriteLine(coockies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine(coockies.Count);
+            Console.WriteLine("All coockies");
+            foreach (var item in coockies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
