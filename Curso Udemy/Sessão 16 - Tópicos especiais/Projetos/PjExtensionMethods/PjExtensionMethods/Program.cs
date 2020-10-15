0eba845c6f4cbefe369096c2ece74bdd3fbb1859
rafaelguerero <rmichelinig@gmail.com>
2020-10-14 22:35:47 -0300

    Extension Methods

---
+++ Curso Udemy/Sessão 16 - Tópicos especiais/Projetos/PjExtensionMethods/PjExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PjExtensionMethods.Extensions;
+
+namespace PjExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 10, 14, 22, 35, 10);
+            string s1 = "Good Morning";
+            Console.WriteLine(dt.ElapsedTime());
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
