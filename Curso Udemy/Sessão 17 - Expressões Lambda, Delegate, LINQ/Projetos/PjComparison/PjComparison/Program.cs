6b87db90ca68eb9c0e10a8494fc1054316bd2c85
rafaelguerero <rmichelinig@gmail.com>
2020-10-21 23:50:39 -0300

    Comparison e Delegate

---
+++ Curso Udemy/Sessão 17 - Expressões Lambda, Delegate, LINQ/Projetos/PjComparison/PjComparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using PjComparison.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace PjComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900));
+            list.Add(new Product("Notebook", 3500));
+            list.Add(new Product("Tablet", 750));
+
+            //Expressão lambda, ordena os produtos por nome
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ Curso Udemy/Sessão 17 - Expressões Lambda, Delegate, LINQ/Projetos/PjDelegate/PjDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PjDelegate.Services;
+
+namespace PjDelegate
+{
+    //Delegate
+    internal delegate double BinaryNumericOperation(double n1, double n2);
+    //Multicast Delegate
+    internal delegate void BinaryNumericOp(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOp ope = CalculationService.ShowSum;
+            ope += CalculationService.ShowMax;
+
+            //double result = op(a, b);
+            double result = op.Invoke(a, b);
+            ope.Invoke(a, b);
+
+            Console.WriteLine(result);            
+        }
+    }
+}
