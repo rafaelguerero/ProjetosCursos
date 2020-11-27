a0e8959b8923bfe57b3879545b70d1f0d612bcb4
rafaelguerero <rmichelinig@gmail.com>
2020-11-26 23:42:24 -0300

    Detalhes e eager loading

--- Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Services/SellerService.cs
+++ Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Services/SellerService.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using ProjetoVendas.Data;
 using ProjetoVendas.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoVendas.Services
 {
@@ -30,7 +29,7 @@ namespace ProjetoVendas.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
