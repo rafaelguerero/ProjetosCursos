6fb3d8167f3cc8caa02e1fdfdb7dae016b87d737
rafaelguerero <rmichelinig@gmail.com>
2020-11-29 22:42:26 -0300

    Custom erro page

--- Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Controllers/SellersController.cs
+++ Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Controllers/SellersController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoVendas.Models;
 using ProjetoVendas.Models.ViewModels;
 using ProjetoVendas.Services;
+using ProjetoVendas.Services.Exceptions;
 
 namespace ProjetoVendas.Controllers
 {
@@ -44,13 +46,13 @@ namespace ProjetoVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided"});
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -68,16 +70,60 @@ namespace ProjetoVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel {Seller = obj, Departments = departments};
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
+            }
+            
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }      
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel {Message = message, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+            return View(viewModel);
         }
     }
 }
--- Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Services/SellerService.cs
+++ Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using ProjetoVendas.Data;
 using ProjetoVendas.Models;
 using Microsoft.EntityFrameworkCore;
+using ProjetoVendas.Services.Exceptions;
 
 namespace ProjetoVendas.Services
 {
@@ -37,6 +38,23 @@ namespace ProjetoVendas.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }            
         }
     }
 }
