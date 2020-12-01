3ffefe2ef68cfd4f8a3786c066fa7cdba952720c
rafaelguerero <rmichelinig@gmail.com>
2020-11-30 23:32:36 -0300

    Busca agrupada

--- Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Models/SalesRecord.cs
+++ Curso Udemy/Sessão 18 - MVC/ProjetoMVC/ProjetoVendas/ProjetoVendas/Models/SalesRecord.cs
@@ -10,6 +10,8 @@ namespace ProjetoVendas.Models
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SalesStatus Status { get; set; }
         public Seller Seller { get; set; }
