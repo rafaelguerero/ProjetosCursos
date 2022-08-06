cb9a0ded96e7dcef534fd0d03c4f2589f91df457
rafaelguerero <rmichelinig@gmail.com>
2022-08-06 00:08:35 -0300

    Criação e configuração projeto angular

--- Projetos/ProEventos/Back/src/ProEventos.API/Startup.cs
+++ Projetos/ProEventos/Back/src/ProEventos.API/Startup.cs
@@ -30,6 +30,7 @@ namespace ProEventos.API
         {
             services.AddDbContext<DataContext>(context => context.UseSqlite(Configuration.GetConnectionString("Default")));
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -51,6 +52,8 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Projetos/ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+})
+export class EventosComponent implements OnInit {
+  public eventos: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe({
+      next: (v) => (this.eventos = v),
+      error: (e) => console.log(e),
+    });
+  }
+}
