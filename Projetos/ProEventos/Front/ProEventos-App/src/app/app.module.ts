92cb4736be26ee2d159edb260d3e36804f6544c3
rafaelguerero <rmichelinig@gmail.com>
2022-08-09 00:17:42 -0300

    Interpolação, diretiva e binding

--- Projetos/ProEventos/Front/ProEventos-App/src/app/app.module.ts
+++ Projetos/ProEventos/Front/ProEventos-App/src/app/app.module.ts
@@ -2,19 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, EventosComponent, PalestrantesComponent],
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent,
+    NavComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Projetos/ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Projetos/ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  public larguraImg: number = 150;
+  public margemImg: number = 2;
+  public ocultarImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista
+      ? this.filtrarEventos(this._filtroLista)
+      : this.eventos;
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,10 +31,25 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public alterarImagem(): void {
+    this.ocultarImagem = !this.ocultarImagem;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe({
-      next: (v) => (this.eventos = v),
+      next: (v) => {
+        (this.eventos = v), (this.eventosFiltrados = this.eventos);
+      },
       error: (e) => console.log(e),
     });
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (e: { tema: string; local: string }) =>
+        e.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        e.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 }
