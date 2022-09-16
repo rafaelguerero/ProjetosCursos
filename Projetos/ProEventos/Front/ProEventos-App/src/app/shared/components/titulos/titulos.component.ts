c590cde81cfe24210e2623f10524345fe378488f
rafaelguerero <rmichelinig@gmail.com>
2022-09-15 23:46:47 -0300

    Bootswatch e componente titulos

--- Projetos/ProEventos/Front/ProEventos-App/src/app/shared/components/titulos/titulos.component.ts
+++ Projetos/ProEventos/Front/ProEventos-App/src/app/shared/components/titulos/titulos.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TitulosComponent implements OnInit {
   @Input() titulo: string = '';
+  @Input() subtitulo: string = 'Desde 2022';
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() botaoListar: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
